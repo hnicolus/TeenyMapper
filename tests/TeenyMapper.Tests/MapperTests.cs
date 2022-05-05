5e45865ddd41ea9636be46bd8b48844aed382c30
Nicolas Maluleke <36539498+hnicolus@users.noreply.github.com>
2022-05-05 17:23:40 +0200

    Update MapperTests.cs

--- tests/TeenyMapper.Tests/MapperTests.cs
+++ tests/TeenyMapper.Tests/MapperTests.cs
@@ -2,12 +2,12 @@ namespace TeenyMapper.Tests;
 public class A
 {
     public int Id { get; set; } = 1;
-    public string Name { get; set; }
+    public string Name { get; set; } =String.Empty;
 }
 public class B
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = String.Empty;
 }
 
 public class MapperTests
@@ -15,7 +15,7 @@ public class MapperTests
     [Fact]
     public void Map_shouldMap_SameProperties()
     {
-        var a = new A { Id = 1, Name = "abc" };
+        var a = new A { Id = 123, Name = "abc" };
         var b =  Mapper.Map<B>(a);
         Assert.Equal(a.Id,b.Id);
         Assert.Equal(a.Name,b.Name);
